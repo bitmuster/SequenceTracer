f193770e0f33a10a1c6ca8de9ad4da60d522a9c5
Michael Abel <micha@proteus.localdomain>
2017-09-17 18:02:23 +0200

    New simple test script

---
+++ tests/simple_script.py
@@ -0,0 +1,22 @@
+
+expected_diagram="""
+seqdiag {
+    edge_length = 140;
+    span_height = 5;
+    None;None_do_stuff;None_do_more_stuff;
+    None -> None_do_stuff [label = "do_stuff"];
+    None_do_stuff -> None_do_more_stuff [label = "do_more_stuff"];
+    None_do_stuff <-- None_do_more_stuff;
+    None <-- None_do_stuff;
+}
+"""
+
+def do_more_stuff():
+    pass
+
+def do_stuff():
+    do_more_stuff()
+
+if __name__ == '__main__':
+
+    do_stuff()
